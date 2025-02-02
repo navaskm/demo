de483435258efc0fd66aab12b481a41fd50bb30f
navaskmsanu <navasnavaskm064@gmail.com>
2025-02-02 11:53:47 -0800

    add module

--- src/app/components/HomePage/SmallProducts/SmallProducts.tsx
+++ src/app/components/HomePage/SmallProducts/SmallProducts.tsx
@@ -1,5 +1,5 @@
 
-import '@/app/styles/homepage/SmallProducts/smallproducts.scss';
+import '@/app/styles/homepage/SmallProducts/smallproducts.module.scss';
 
 import { fetchProduct } from "@/app/DataFetching/productData"
 import Link from "next/link";

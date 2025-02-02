3390bcda465a6bedd760f775ab3f90a152450cd7
navaskmsanu <navasnavaskm064@gmail.com>
2025-02-02 13:54:28 -0800

    created new css file

--- src/app/components/HomePage/SmallProducts/SmallProducts.tsx
+++ src/app/components/HomePage/SmallProducts/SmallProducts.tsx
@@ -1,4 +1,5 @@
-import '@/app/styles/homepage/SmallProducts/smallproducts.scss';
+//import '@/app/styles/homepage/SmallProducts/smallproducts.scss';
+import '@/app/Style/HomePage/SmallProducts/SmallProducts.scss';
 
 import { fetchProduct } from "@/app/DataFetching/productData"
 import Link from "next/link";

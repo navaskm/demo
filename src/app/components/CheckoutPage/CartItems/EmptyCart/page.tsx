1c31c1dc217b3772fd6e841afe050f8e85f9d355
navaskmsanu <navasnavaskm064@gmail.com>
2025-02-02 14:12:02 -0800

    add image heaight

--- src/app/components/CheckoutPage/CartItems/EmptyCart/page.tsx
+++ src/app/components/CheckoutPage/CartItems/EmptyCart/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 const EmptyCart = () => {
   return (
     <div className="empty-cart-container">
-        <img src="/EmptyCart/emptyImage.png" alt="" />
+        <img src="/EmptyCart/emptyImage.png" alt="" width={500} height={300}/>
         <h5>Your cart is empty!</h5>
         <p>Add items to it now.</p>
         <Link href="/" style={{textDecoration:"none",color:"white"}}>Start Shopping</Link>

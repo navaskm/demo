f7bb6f17c49315d63a8a8e36a53334ef9055865c
navaskmsanu <navasnavaskm064@gmail.com>
2025-02-02 14:45:21 -0800

    Import Image

--- src/app/components/CheckoutPage/CartItems/EmptyCart/page.tsx
+++ src/app/components/CheckoutPage/CartItems/EmptyCart/page.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link"
+import Image from "next/image"
 
 const EmptyCart = () => {
   return (
     <div className="empty-cart-container">
-        <img src="/EmptyCart/emptyImage.png" alt="" width={500} height={300}/>
+        <Image src="/EmptyCart/emptyImage.png" alt="" width={500} height={300}/>
         <h5>Your cart is empty!</h5>
         <p>Add items to it now.</p>
         <Link href="/" style={{textDecoration:"none",color:"white"}}>Start Shopping</Link>

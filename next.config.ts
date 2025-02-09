fdb988f4f2faead15363fd4e6f427bc4a319a996
navaskmsanu <navasnavaskm064@gmail.com>
2025-02-08 21:50:08 -0800

    export page.tsx

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,8 @@ import type { NextConfig } from "next";
 const nextConfig:NextConfig = {
   typescript:{
     ignoreBuildErrors:true,
-  }
+  },
+  pageExtensions: ['page.tsx', 'page.ts'],
 };
 
 export default nextConfig;

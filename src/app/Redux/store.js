f3feb9c79b0400ac9715b88f2d58fad0b2afd5a8
navaskmsanu <navasnavaskm064@gmail.com>
2025-02-10 13:00:06 -0800

    change store imports

--- src/app/Redux/store.js
+++ src/app/Redux/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import cartReducer from "../components/HomePage/navbar/CartLogo/cartlogoslice";
-import cartItemsReducer from "../components/SelectedPage/ImageDisplay/AddToCartBtn/cartslice";
-import deliveryDateReducer from "../components/CheckoutPage/CartItems/cartitems";
+// import cartReducer from "../components/HomePage/navbar/CartLogo/cartlogoslice";
+// import cartItemsReducer from "../components/SelectedPage/ImageDisplay/AddToCartBtn/cartslice";
+// import deliveryDateReducer from "../components/CheckoutPage/CartItems/cartitems";
+import cartReducer from "@/app/components/HomePage/navbar/CartLogo/cartlogoslice";
+import cartItemsReducer from '@/app/components/SelectedPage/ImageDisplay/AddToCartBtn/cartslice'
+import deliveryDateReducer from '@/app/components/CheckoutPage/CartItems/cartitems'
 
 const store = configureStore({
   reducer: {
